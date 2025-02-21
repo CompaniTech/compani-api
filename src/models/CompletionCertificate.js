e8a820066c2fa366617fc35cbe59137e125e279c
Manon Palin <manonpalin@mbp-de-manon.home>
2025-02-21 09:38:33 +0100

    COM-3910 : create CompletionCertificate model

---
+++ src/models/CompletionCertificate.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const CompletionCertificateSchema = mongoose.Schema({
+  course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true, immutable: true },
+  trainee: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, immutable: true },
+  month: {
+    type: String,
+    required: true,
+    immutable: true,
+    validate: /^([0]{1}[1-9]{1}|[1]{1}[0-2]{1})-[2]{1}[0]{1}[0-9]{2}$/
+  },
+});
+
+module.exports = mongoose.model('CompletionCertificate', CompletionCertificateSchema);
