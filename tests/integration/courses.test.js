1ee6b68d3c469b7d46b92a54915245b08885d4cc
Aude <aude@compani.fr>
2025-03-03 16:13:28 +0100

    COM-3920: add integration test update company representative in single course

--- tests/integration/courses.test.js
+++ tests/integration/courses.test.js
@@ -1754,6 +1754,22 @@ describe('COURSES ROUTES - PUT /courses/{_id}', () => {
       expect(courseUpdated).toEqual(1);
     });
 
+    it('should update company representative on single course', async () => {
+      const payload = { companyRepresentative: coach._id };
+
+      const response = await app.inject({
+        method: 'PUT',
+        url: `/courses/${coursesList[25]._id}`,
+        headers: { Cookie: `alenvi_token=${authToken}` },
+        payload,
+      });
+
+      expect(response.statusCode).toBe(200);
+
+      const courseUpdated = await Course.countDocuments({ _id: coursesList[25]._id, ...payload });
+      expect(courseUpdated).toEqual(1);
+    });
+
     it('should update company representative and set as contact directly for INTRA course', async () => {
       const payload = { companyRepresentative: coachFromOtherCompany._id, contact: coachFromOtherCompany._id };
       const response = await app.inject({
