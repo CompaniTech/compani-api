25600dfda563f20c357153a7cdf571931a62d169
ulysse <ulysse@alenvi.io>
2025-07-31 15:52:58 +0200

    COM-4025: rename tests and change variable type

--- tests/unit/helpers/attendanceSheets.test.js
+++ tests/unit/helpers/attendanceSheets.test.js
@@ -428,7 +428,7 @@ describe('create', () => {
     sinon.assert.notCalled(attendanceSheetFindOneAndUpdate);
   });
 
-  it('should upload trainer signature and update attendance sheets for INTER', async () => {
+  it('hould get existing trainer signature and update attendance sheet for INTER', async () => {
     const courseId = new ObjectId();
     const traineesId = [new ObjectId(), new ObjectId()];
     const companyId = new ObjectId();
