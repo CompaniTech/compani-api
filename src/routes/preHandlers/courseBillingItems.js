0b4bcb90289a85b7adee2e2b2e71d61e064eff70
Aude <aude@MacBook-Pro-de-Aude.local>
2025-08-05 16:54:31 +0200

    COM-4015: create delete route for courseBillingItem

--- src/routes/preHandlers/courseBillingItems.js
+++ src/routes/preHandlers/courseBillingItems.js
@@ -1,4 +1,5 @@
 const Boom = require('@hapi/boom');
+const has = require('lodash/has');
 const CourseBillingItem = require('../../models/CourseBillingItem');
 const translate = require('../../helpers/translate');
 
@@ -9,6 +10,21 @@ exports.authorizeCourseBillingItemCreation = async (req) => {
     .countDocuments({ name: req.payload.name }, { limit: 1 })
     .collation({ locale: 'fr', strength: 1 });
   if (nameAlreadyExists) throw Boom.conflict(translate[language].courseBillingItemExists);
+
+  return null;
+};
+
+exports.authorizeBillingItemsDeletion = async (req) => {
+  const { credentials } = req.auth;
+
+  const courseBillingItems = await CourseBillingItem
+    .countDocuments(req.params)
+    .populate({ path: 'courseBillCount', options: { isVendorUser: has(credentials, 'role.vendor') } })
+    .leaan();
+
+  if (!courseBillingItems) throw Boom.notFound();
+
+  if (courseBillingItems.courseBillCount) throw Boom.forbidden();
 
   return null;
 };
