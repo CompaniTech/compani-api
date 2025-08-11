06b74800d77bce49a168ed22a8811b4d42b53cdb
Manon Palin <manonpalin@mbp-de-manon.home>
2025-08-11 14:58:04 +0200

    COM-4040 - integration tests

--- tests/integration/seed/userCompaniesSeed.js
+++ tests/integration/seed/userCompaniesSeed.js
@@ -181,7 +181,7 @@ const company = {
   _id: DETACHMENT_ALLOWED_COMPANY_IDS[0],
   name: 'Structure dans laquelle on peut detacher des apprenants',
   iban: 'FR3514508000505917721779B12',
-  bic: 'RTYUIKJHBFRG',
+  bic: 'ERTYFRPP',
   folderId: '0987654321',
   directDebitsFolderId: '1234567890',
   customersFolderId: 'mnbvcxz',
