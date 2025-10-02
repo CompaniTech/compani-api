35f1654493085104d316d8f5c69ccb7154c5b2ee
Manon Palin <manonpalin@mbp-de-manon.home>
2025-10-02 14:44:25 +0200

    COM-4063 - add timestamps in xmlSEPAFileInfos model

--- src/models/XmlSEPAFileInfos.js
+++ src/models/XmlSEPAFileInfos.js
@@ -4,7 +4,7 @@ const { validateQuery } = require('./preHooks/validate');
 const XmlSEPAFileInfosSchema = mongoose.Schema({
   coursePayments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'CoursePayment', required: true }],
   name: { type: String, required: true, unique: true },
-});
+}, { timestamps: true });
 
 XmlSEPAFileInfosSchema.pre('find', validateQuery);
 module.exports = mongoose.model('XmlSEPAFileInfos', XmlSEPAFileInfosSchema);
