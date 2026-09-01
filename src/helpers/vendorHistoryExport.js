1d81c88353f5f4e1e59c09f5b57bf87bba49f38a
ulysse <ulysse@alenvi.io>
2026-09-01 18:16:34 +0200

    COM-4280: add column in bill

--- src/helpers/vendorHistoryExport.js
+++ src/helpers/vendorHistoryExport.js
@@ -114,20 +114,27 @@ const getAttendancesCountInfos = (course) => {
 
 const getBillsInfos = (course) => {
   const validatedBillsWithoutCreditNote = course.bills.filter(bill => !bill.courseCreditNote && bill.billedAt);
+  const draftBills = course.bills.filter(bill => !bill.billedAt);
 
   const payerList =
     [...new Set(validatedBillsWithoutCreditNote.map(bill => get(bill, 'payer.name')))]
       .sort((a, b) => a.localeCompare(b))
       .toString();
   const computedAmounts = validatedBillsWithoutCreditNote.map(bill => CourseBillHelper.computeAmounts(bill));
+  const draftComputedAmounts = draftBills.map(bill => CourseBillHelper.computeAmounts(bill));
+  const draftNetExclTaxes = draftComputedAmounts.length
+    ? draftComputedAmounts.map(amount => amount.netExclTaxes).reduce((acc, value) => acc + value, 0)
+    : '';
+
   const amountsInfos = validatedBillsWithoutCreditNote.length
     ? {
       netInclTaxes: computedAmounts.map(amount => amount.netInclTaxes).reduce((acc, value) => acc + value, 0),
       netExclTaxes: computedAmounts.map(amount => amount.netExclTaxes).reduce((acc, value) => acc + value, 0),
+      draftNetExclTaxes,
       paid: computedAmounts.map(amount => amount.paid).reduce((acc, value) => acc + value, 0),
       total: computedAmounts.map(amount => amount.total).reduce((acc, value) => acc + value, 0),
     }
-    : { netExclTaxes: '', netInclTaxes: '', paid: '', total: '' };
+    : { netExclTaxes: '', netInclTaxes: '', paid: '', total: '', draftNetExclTaxes };
 
   if ([INTRA, SINGLE].includes(course.type)) {
     const billsCountForExport = {
@@ -231,6 +238,7 @@ const formatCourseForExport = async (
     payerList,
     netInclTaxes,
     netExclTaxes,
+    draftNetExclTaxes,
     paid,
     total,
   } = getBillsInfos(course);
@@ -322,6 +330,7 @@ const formatCourseForExport = async (
     'Nombre de factures validées': billsCountForExport.validatedBillsCount,
     'Nombre de factures attendues': billsCountForExport.expectedBillsCount,
     Facturée: isBilled ? 'Oui' : 'Non',
+    'Montant à facturer HT': UtilsHelper.formatFloatForExport(draftNetExclTaxes),
     'Montant facturé HT': UtilsHelper.formatFloatForExport(netExclTaxes),
     'Montant facturé TTC': UtilsHelper.formatFloatForExport(netInclTaxes),
     'Montant réglé': UtilsHelper.formatFloatForExport(paid),
